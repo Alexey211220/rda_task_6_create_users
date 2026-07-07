0afc379353c4180b2648c505914b4d0cd4dc9edd
alexey211204 <alexey.zhuravel2004@gmail.com>
2026-07-07 13:32:55 +0200

    final solution

--- task.sql
+++ task.sql
@@ -1,5 +1,8 @@
+-- This SQL script creates 1 database user and give him only CRUD permissions on the ShopDB database. 
 CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
-GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB. * TO 'webappuser'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
 
+
+-- This SQL script creates 1 database user and give him all permissions on the ShopDB database.
 CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
-GRANT ALL PRIVILEGES ON ShopDB. * TO 'deploymentuser'@'%';
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
